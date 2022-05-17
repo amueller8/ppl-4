505dcc55583476804993da82914839f7e695b82e
jmanjoyce <jjoyce@bowdoin.edu>
2022-05-17 14:48:02 -0400

    push stuff

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,17 @@
 module UsersHelper
+    def get-color(popularity)
+        if popularity > 50
+            arr = array.new(3)
+            arr[0] = 255
+            arr[1] = 0
+            arr[2] = 255 - ((popularity-50)*(255/50))
+            puts arr
+        else
+            arr = array.new(3)
+            arr[0] = ((popularity*(255/50)))
+            arr[1] = 0
+            arr[2] = 255
+            puts arr
+        end
+    end
 end
