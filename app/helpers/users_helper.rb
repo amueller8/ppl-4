dca3ef86c1c47581bc6edd66cd7336c3f0fd1aa5
Abby Mueller <amueller@bowdoin.edu>
2022-05-17 14:53:56 -0400

    fix typos

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,17 +1,17 @@
 module UsersHelper
-    def get-color(popularity)
+    def get_color(popularity)
         if popularity > 50
-            arr = array.new(3)
+            arr = Array.new(3)
             arr[0] = 255
             arr[1] = 0
             arr[2] = 255 - ((popularity-50)*(255/50))
-            puts arr
+            arr
         else
-            arr = array.new(3)
+            arr = Array.new(3)
             arr[0] = ((popularity*(255/50)))
             arr[1] = 0
             arr[2] = 255
-            puts arr
+            arr
         end
     end
 end
